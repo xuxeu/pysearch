4346a7709bdbb7f313da8a0eb431ccba21c411b7
xuxeu <1195628909@qq.com>
2017-05-14 05:38:20 -0400

    shell基本认识与快捷键

---
+++ shell/hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#上面不可省略，表示为sh脚本，调用bash
+#the first program
+#代表注释
+#执行该脚本有两种方式：
+#1.bash hello.sh
+#2.通过chmod 755 hello.sh给它赋予执行权限，然后，使用./hello.sh
+echo -e "\e[1;34m 南方有佳人！\e[0m"
---
+++ shell/shell.md
@@ -0,0 +1,7 @@
+shell
+shell是命令行解释器，用于把用户的输入解释成系统可以识别的机器语言。
+
+shell主要有两种语法类型，分为bourne和c。
+这两种语法彼此不兼容。
+bourne家族主要包括sh，ksh，bash，psh，zsh。
+c家族主要包括：csh，tcsh。
